a4bb0d0e181486fd02895aa5dfa3909999e032dd
jschweizer78 <jschweizer78@gmail.com>
2016-03-27 01:37:44 -0700

    orders

--- models/order.go
+++ models/order.go
@@ -4,10 +4,27 @@ package models
 type Order struct {
 	ID        string
 	PatientID string
-	Items     []OrderItem
+	Items     []*OrderItem
+	Total     float64
 }
 
 // OrderItem reperesents an orderItem
 type OrderItem struct {
-	Product InventoryItem
+	Product  InventoryItem
+	Quantity int
+	Total    float64
+}
+
+// TotalOrder return the order total
+func (o *Order) TotalOrder() float64 {
+	var total float64
+	for _, item := range o.Items {
+		total += item.Total
+	}
+	return total
+}
+
+// TotalItem returns the total for this line item
+func (ot *OrderItem) TotalItem() float64 {
+	return float64(ot.Quantity) * ot.Product.PriceUSD
 }
--- models/product.go
+++ models/product.go
@@ -1,10 +1,11 @@
 package models
 
-// Product reperesents a product
+// InventoryItem reperesents a product in inventory
 type InventoryItem struct {
 	ID          string
 	Name        string
 	Description string
+	PriceUSD    float64
 	Category    string
 	ImageURL    string
 	Quantity    int
